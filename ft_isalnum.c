523d0cef1fd7d860c55775ae824f2b2e2467dc41
biltaraki <btaraki056@gmail.com>
2025-09-29 12:55:16 +0100

    ok

---
+++ ft_isalnum.c
@@ -0,0 +1,9 @@
+#include "libft.h"
+int     ft_isalnum(int c)
+{
+    if((c <= 'Z' && c >= 'A') || (c <= 'z' && c >= 'a') || (c <= '9' && c >= '0'))
+    {
+        return 1;
+    }
+    return 0;
+}
