0a74298e0f268ee5eedddadc9a98a8da8bede784
biltaraki <btaraki056@gmail.com>
2025-10-01 14:50:34 +0100

    libft

--- libft.h
+++ libft.h
@@ -22,8 +22,8 @@ size_t  ft_strlen(const char *s);
 char    *ft_strchr(const char *s, int c);
 int     ft_strncmp(const char *s1, const char *s2, size_t n);
 char    *ft_strrchr(const char *s, int c);
-    size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
-    size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
 char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
 char    *ft_strdup(const char *s1);
 
