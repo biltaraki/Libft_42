478faff223878dc1f9363e937e6c43e27860d0cf
biltaraki <btaraki056@gmail.com>
2025-10-22 14:11:48 +0100

    feat: implement memcmp memory comparison function
    - Add function to compare memory areas byte by byte
    - Returns difference between first differing bytes
    - Returns 0 if all bytes are equal
    - Handles unsigned character comparison

---
+++ ft_memcmp.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*a1;
+	const unsigned char	*a2;
+	size_t				i;
+
+	a1 = (const unsigned char *)s1;
+	a2 = (const unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (a1[i] != a2[i])
+			return (a1[i] - a2[i]);
+		i++;
+	}
+	return (0);
+}
