26977ab29226dedfde6ca211f6dc87acbb209f91
biltaraki <btaraki056@gmail.com>
2025-10-23 11:07:05 +0100

    feat: implement strdup string duplication function
    - Add function to duplicate string with malloc
    - Allocates memory and copies source string
    - Returns pointer to new string or NULL on failure
    - Includes null terminator in duplication

---
+++ ft_strdup.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	i;
+	size_t	len;
+	char	*array;
+
+	len = ft_strlen(s1) + 1;
+	array = (char *)malloc(sizeof(char) * len);
+	if (array == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len - 1)
+	{
+		array[i] = s1[i];
+		i++;
+	}
+	array[i] = '\0';
+	return (array);
+}
