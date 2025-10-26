d74c545452efb6958a2c0317a11a228f58bc5f5f
biltaraki <btaraki1337@gmail.com>
2025-10-26 20:00:20 +0100

    add strjoin🪶​

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -26,7 +26,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 || !s2)
 		return (NULL);
-	len_s1 = ft_strlen(s1);
+	len_s1 = ft_strlen(s1);//header
 	len_s2 = ft_strlen(s2);
 	arr = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
 	if (arr == NULL)
