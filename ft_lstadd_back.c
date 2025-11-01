0ec8e7f51f2c8db342ed3c0d3d4874fe54d451af
biltaraki <btaraki1337@gmail.com>
2025-11-01 13:34:17 +0100

    add lstadd_back🪶

---
+++ ft_lstadd_back.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*node;
+
+	if (!new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	node = *lst;
+	while (node->next)
+		node = node->next;
+	node->next = new;
+	// Don't set new->next = NULL here
+	// Let the node creation handle that
+}
