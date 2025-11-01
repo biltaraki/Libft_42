81fb8580c2b577d9ba59057a0b4e35dbc09bed67
biltaraki <btaraki1337@gmail.com>
2025-11-01 14:33:33 +0100

    add lstclear🪶

---
+++ ft_lstclear.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*current;
+	t_list	*temp;
+
+	if (!lst || !*lst || !del)
+		return ;
+	current = *lst;
+	while (current)
+	{
+		temp = current->next;
+		del(current->content);
+		free(current);
+		current = temp;
+	}
+	*lst = NULL;
+}
