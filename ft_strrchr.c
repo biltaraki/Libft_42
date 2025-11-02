b975742413628940e8fbf4460a1bbf48f8e0a105
biltaraki <btaraki1337@gmail.com>
2025-11-02 11:06:55 +0100

    add strrchr🪶

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -2,13 +2,13 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	size_t	len;
+	size_t	length;
 
-	len = ft_strlen(s) + 1;
-	while (len--)
+	length = ft_strlen(s) + 1;
+	while (length--)
 	{
-		if (s[len] == (char)c)
-			return ((char *)&s[len]);
+		if (s[length] == (char)c)
+			return ((char *)&s[length]);
 	}
 	return (NULL);
 }
