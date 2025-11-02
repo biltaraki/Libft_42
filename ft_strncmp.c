0fb919365ec02fd2d0b96c6eb03a084bb4171184
biltaraki <btaraki1337@gmail.com>
2025-11-02 11:05:15 +0100

    add strncmp🪶

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -2,16 +2,16 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t	index;
 
-	i = 0;
+	index = 0;
 	if (n == 0)
 		return (0);
-	while (s1[i] && s2[i] && i < n - 1)
+	while (s1[index] && s2[index] && index < n - 1)
 	{
-		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		i++;
+		if (s1[index] != s2[index])
+			return ((unsigned char)s1[index] - (unsigned char)s2[index]);
+		index++;
 	}
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return ((unsigned char)s1[index] - (unsigned char)s2[index]);
 }
